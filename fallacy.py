40236a27841bebfe68b11f713756805a0fb4f1bd
VIBHOR SHUKLA <shukla.vibhor495@gmail.com>
2024-04-05 16:52:03 +0530

    Adding cleaner implementation of integration of cam with model

--- fallacy.py
+++ fallacy.py
@@ -2,7 +2,7 @@ import cv2
 from ultralytics import YOLO
 
 model = YOLO("Path to the model")
-cam = "http://192.168.137.197:8080/video" #IP webcam link
+cam = "http://192.123.456.789:8080/video" #IP webcam link
 cap = cv2.VideoCapture(0)
 cap.open(cam)
 print("check ===>", cap.isOpened())
@@ -14,10 +14,7 @@ while cap.isOpened():
         frame = cv2.resize(frame, (700, 700))
         results = model(frame)
         for det in results:
-            # Assuming det is the class label (e.g., "rotten")
             cls = det
-            # cv2.putText(frame, f"{cls}({conf:.2f})", (int(x_min), int(y_min) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-            #             (255, 0, 0), 2)
         cv2.imshow("WebCam", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
